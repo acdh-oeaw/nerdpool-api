a175ff140c94dc5f3af69f78834882746e2a5bec
csae8092 <peter.andorfer@oeaw.ac.at>
2021-03-23 15:43:34 +0100

    added fundament theme

--- nerdpool/settings.py
+++ nerdpool/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
-    # 'django_extensions',
+    'django_extensions',
     'django_filters',
     'archiv',
 ]
