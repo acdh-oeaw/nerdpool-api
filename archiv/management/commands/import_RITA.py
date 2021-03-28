a31b1282a6eaac71c08f271f1990d24ff1878bef
csae8092 <peter.andorfer@oeaw.ac.at>
2021-03-28 19:16:46 +0200

    rita data and import script added

---
+++ archiv/management/commands/import_RITA.py
@@ -0,0 +1,43 @@
+import json
+
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from archiv.models import NerSource, NerSample
+
+source_json = {
+    "title": "RITA",
+    "info": {
+        "creators": [
+            {
+                "name": "Michael Span",
+            }
+        ],
+        "description": "Verfachbücher"
+    }
+}
+
+
+class Command(BaseCommand):
+
+    help = "Fetches NERSamples short_samples.json"
+
+    def handle(self, *args, **kwargs):
+        for x in NerSource.objects.filter(title=source_json['title']):
+            x.delete()
+        source = NerSource.objects.create(
+            title=source_json['title'],
+            info=source_json['info']
+        )
+        filepath = 'rita.jsonl'
+        with open(filepath) as fp:
+            for x in tqdm(fp.readlines(), total=924):
+                data = json.loads(x)
+                text = data['text']
+                ner_exist = bool(data['entities'])
+                NerSample.objects.create(
+                    ner_text=text,
+                    ner_sample=data,
+                    ner_ent_exist=ner_exist,
+                    ner_source=source
+                )
