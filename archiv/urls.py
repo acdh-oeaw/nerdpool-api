317ff34fbec6ce861197fdf694d82431c4e42c38
csae8092 <peter.andorfer@oeaw.ac.at>
2021-03-31 13:33:25 +0200

    imprint added

--- archiv/urls.py
+++ archiv/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='index'),
+    path('imprint', views.ImprintView.as_view(), name='imprint'),
 ]
--- archiv/views.py
+++ archiv/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.views.generic.base import TemplateView
 
 from archiv.models import NerSource, NerSample
@@ -12,4 +13,21 @@ class HomePageView(TemplateView):
         context['ner_source'] = NerSource.objects.all()
         context['ner_source_count'] = NerSource.objects.all().count()
         context['ner_samples_count'] = NerSample.objects.all().count()
+        return context
+
+
+class ImprintView(TemplateView):
+    template_name = 'archiv/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        r = requests.get("https://shared.acdh.oeaw.ac.at/acdh-common-assets/api/imprint.php?serviceID=19058")
+
+        if r.status_code == 200:
+            context['imprint_body'] = f"{r.text}"
+        else:
+            context['imprint_body'] = """
+            On of our services is currently not available. Please try it later or write an email to
+            acdh@oeaw.ac.at
+            """
         return context
