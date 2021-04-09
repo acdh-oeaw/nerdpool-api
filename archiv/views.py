7f96f16bb3a4bb7996f2187be0b87e1c0ce912d2
csae8092 <peter.andorfer@oeaw.ac.at>
2021-04-09 11:46:18 +0200

    fix #2

--- archiv/views.py
+++ archiv/views.py
@@ -1,4 +1,7 @@
 import requests
+from django.contrib.admin.utils import flatten
+from collections import Counter
+from django.db.models import Count
 from django.views.generic.base import TemplateView
 
 from archiv.models import NerSource, NerSample
@@ -10,9 +13,29 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        ner_source = list(NerSource.objects.values_list('title', 'info').annotate(num_samples=Count('nersample')))
+        dict_list = [
+            {
+                'name': x[0],
+                'y': x[2],
+                'drilldown': x[0]
+            } for x in ner_source
+        ]
+        ner_titles = [x[0] for x in list(NerSource.objects.values_list('title'))]
+        drilldown = []
+        for x in ner_titles:
+            item = {
+                'name': x,
+                'id': x,
+            }
+            samples = NerSample.objects.filter(ner_ent_exist=True).filter(ner_source__title=x)
+            ents = [x['ner_sample__entities'] for x in samples.values('ner_sample__entities')]
+            ents = Counter([x[2] for x in (flatten(ents))])
+            item['data'] = [[k, v] for k, v in dict(ents).items()]
+            drilldown.append(item)
         context['ner_source'] = NerSource.objects.all()
-        context['ner_source_count'] = NerSource.objects.all().count()
-        context['ner_samples_count'] = NerSample.objects.all().count()
+        context['drilldown'] = drilldown
+        context['dict_list'] = dict_list
         return context
 
 
